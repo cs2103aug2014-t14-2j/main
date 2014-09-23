cc46ff0d2a1f24d403ee4f244a82a94a240dbb90
Nelson <nellystix@197-167.priv23.nus.edu.sg>
2014-09-24 03:07:39 +0800

    Added the functions: doAddTask(), confirmAddTask(), doEditTask() and confirmEditTask()
    Need clarification with regard to the doEditTask() method.

--- cs2103/ezC.java
+++ cs2103/ezC.java
@@ -1,9 +1,12 @@
 package cs2103;
 
+import java.util.Date;
+import java.util.List;
+
 public class ezC {
 	
 	// NATALIE - FileIoStream relies on your pre-written FileIoStream Class?
-	Lisk<Task> totalTaskList;
+	static List<Task> totalTaskList;
 	String fileName;
 	FileIoStream fileStream (given fileName)
 	List<String> Read();
@@ -29,11 +32,21 @@ public class ezC {
 	// Deny duplicates to be added 
 	public static void doAddTask(Task toBeAdded) {
 		
+		if(!totalTaskList.contains(toBeAdded)) {	// If the list doesn't contain this task, add it
+			totalTaskList.add(toBeAdded);
+		}
+		totalTaskList.sort();	// Task class needs to implement comparable and do an overriding method
+								// for compareTo() to sort by name/deadline/etc.
+								// Maybe have another method to purely deal with sorting?
+		fileStream.write(totalTaskList);	// Using Natalie's FileIO class to write the task list back
+											// to the file
 	}
 	
 	// NELSON
 	// Print a message to user indicating task X was added
 	public static void confirmAddTask(Task addedTaskToPrintToUser) {
+		
+		System.out.println(addedTaskToPrintToUser.name() + " has been added to your task list.");	// Should I throw this to a specific print funtion? Seems retarded
 		
 	}
 	
@@ -45,11 +58,21 @@ public class ezC {
 	// Should REJECT DUPLICATE entries
 	public static Task doEditTask(Task toEdit, String editedContent) {
 		
+		if(totalTaskList.contains(toEdit)) {
+			Task editedTask = totalTaskList.get(indexOf(toEdit));	// Gets the task that we want to edit from the task list
+			// What am I editing here?
+		}
+		else {
+			System.out.println("The task \"" + toEdit.name + "\" that you are trying to edit does not exist in the task list.");	// Should this be in a separate method too?
+		}
+		
 	}
 	
 	// NELSON
 	// Prints the task that got edited
 	public static void confirmEditTask(Task oldTask, Task newTask) {
+		
+		System.out.println("Your task \"" + newTask.name() + "\" has been edited.");
 		
 	}
 	
