2d5bf385eb73a1318efb70e3788bca3301cd9860
Yui Wei <yuiwei@Yuis-MacBook-Pro.local>
2014-09-30 16:18:25 +0800

    determineDate, findTask, doDeleteTask and confirmDeleteTask

--- cs2103/ezC.java
+++ cs2103/ezC.java
@@ -13,9 +13,56 @@ public class ezC {
 	
 	// YUI WEI
 	public static Date determineDate(String dateString) {
-		return null;
+		String month, day, year;
+		int mm, dd, yyyy;
+		if (dateString.contains(",")) {
+			int space = dateString.indexOf(' ');
+			int comma = dateString.indexOf(',');
+			month = dateString.substring(0, space);
+			day = dateString.substring(space + 1, comma);
+			year = dateString.substring(comma);
+			mm = monthInteger(month);
+		} else if (dateString.contains("/")) {
+			String dateStr[] = dateString.split("/", 3);
+			day = dateStr[0];
+			month = dateStr[1];
+			year = dateStr[2];
+			mm = Integer.parseInt(month);
+		} else {
+			String dateStr[] = dateString.split(" ", 3);
+			day = dateStr[0];
+			month = dateStr[1];
+			year = dateStr[2];
+			mm = Integer.parseInt(month);
+		}
+		dd = Integer.parseInt(day);
+		yyyy = Integer.parseInt(year);
+		
+		Date userDate = new Date(dd, mm, yyyy);
+		return userDate;
 	}
-	
+	//this should probably be made into method in the Date class.
+	static int monthInteger(String month1) {
+		int monthNum;
+		switch (month1.toLowerCase()) {
+			case "january" :		monthNum = 1; 				break;
+			case "february":		monthNum = 2;				break;
+			case "march":			monthNum = 3;				break;
+			case "april":			monthNum = 4;				break;
+			case "may":				monthNum = 5;				break;
+			case "june":			monthNum = 6;				break;
+			case "july":			monthNum = 7;				break;
+			case "august":			monthNum = 8;				break;
+			case "september":		monthNum = 9;				break;
+			case "october":			monthNum = 10;				break;
+			case "november":		monthNum = 11;				break;
+			case "december":		monthNum = 12;				break;
+			default: 				monthNum = 0;				break;
+		}
+		return monthNum;
+	}
+
+
 	// NATALIE
 	public static List<Task> decipherFileContents(List<String> fileContent) {
 		List<Task> taskList = TaskFileReader.getAllTasks(fileContent);
@@ -79,7 +126,17 @@ public class ezC {
 	
 	// YUI WEI
 	public static List<Task> findTask(String taskName) {
-		
+		List<Task> matches = new List<Task>();
+		for (int i=0; i<totalTaskList.size(); i++) {
+			Task cur = totalTaskList.get(i);
+			if (cur.getName().contains(taskName)) {
+				matches.add(cur);
+			}
+		}
+		if (matches.size() == 0) {
+			System.out.println("None found."); //need a standard for this.
+		}
+		return matches;
 	}
 	
 	// VERNON
@@ -89,12 +146,12 @@ public class ezC {
 	
 	// YUI WEI
 	public static Task doDeleteTask(Task taskToDelete) {
-		
+		totalTaskList.remove(toDelete);
 	}
 	
 	// YUI WEI
 	public static void confirmDeleteTask(Task deletedTaskToPrintToUser) {
-		
+				System.out.println(deleted.toString + "has been deleted.");
 	}
 	
 	// VERNON
