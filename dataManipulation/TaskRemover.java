e06b0992aa095b76cb25452d35fce5370df17170
Yui Wei <yuiwei@179-200.priv23.nus.edu.sg>
2014-10-02 23:24:26 +0800

    TaskRemover: No longer taking in CommandComponents, instead taking in Task as parameter

--- dataManipulation/TaskRemover.java
+++ dataManipulation/TaskRemover.java
@@ -1,18 +1,30 @@
 package dataManipulation;
 
+
+import globalClasses.Date;
 import globalClasses.Task;
 import globalClasses.ezC;
+import fileIo.FileIo;
 
 public class TaskRemover {
 	// YUI WEI
 	
-	public static Task doDeleteTask(Task taskToDelete) {
-		ezC.totalTaskList.remove(toDelete);
-		IoStream.rewriteFile(totalTaskList);
+	public static Task doDeleteTask(List<CommandComponents> cc, FileIo IoStream) {
+		Task lookingFor = new Task(cc.get(0), cc.get(1), cc.get(4), cc.get(5), cc.get(2), cc.get(3));
+		boolean flag = false;
+		for (Task t : ezC.totalTaskList) {
+			if (t.equals(lookingFor)) {
+				flag = true;
+				ezC.totalTaskList.remove(t);
+				IoStream.rewriteFile(ezC.totalTaskList);
+			}
+		}
+		if(flag = false) {
+			//print error message
+			lookingFor = null;
+		}
 
-		//search through list (call ExactSearch)
-		//if found, remove
-		//else call print error message function.
+		return lookingFor;
 	}
 	
 	// YUI WEI
