001272532bcd061053229d7058a35d1dba84b646
Yui Wei <yuiwei@179-200.priv23.nus.edu.sg>
2014-10-02 22:53:29 +0800

    taskRemover stuff + comment in CommandInterpreter: this is how TaskRemover and TaskEditor will be using the List<CommandComponents> which CommandInterpreter should create.

--- dataManipulation/TaskRemover.java
+++ dataManipulation/TaskRemover.java
@@ -1,13 +1,26 @@
 package dataManipulation;
 
-import java.util.List;
-
 import globalClasses.Task;
+import globalClasses.ezC;
 
 public class TaskRemover {
 	// YUI WEI
-	public static Task doDeleteTask(Task taskToDelete, List<Task> totalTaskList) {
-		totalTaskList.remove(taskToDelete);			
-		return null;
+	
+	public static Task doDeleteTask(Task taskToDelete) {
+		ezC.totalTaskList.remove(toDelete);
+		IoStream.rewriteFile(totalTaskList);
+
+		//search through list (call ExactSearch)
+		//if found, remove
+		//else call print error message function.
 	}
+	
+	// YUI WEI
+	public static void confirmDeleteTask(Task deletedTaskToPrintToUser) {
+				System.out.println(deleted.toString + "has been deleted.");
+	}
+
+	//other things to maybe add:
+	//	+ delete by category
+	//	+ delete by keyword
 }
--- userInterface/CommandInterpreter.java
+++ userInterface/CommandInterpreter.java
@@ -1,5 +1,11 @@
 package userInterface;
 
 public class CommandInterpreter {
-
+	//List of CommandComponents: 
+	// list[0]: name
+	// list[1]: category
+	// list[2]: start date
+	// list[3]: end date
+	// list[4]: location
+	// list[5]: note
 }
