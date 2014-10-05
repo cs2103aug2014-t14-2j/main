059371b0946afc184ce3fc6ecfbf0c01f1f3c704
Nelson <nellystix@191-37.priv23.nus.edu.sg>
2014-10-05 18:05:13 +0800

    TaskAdder() now functional. Takes in the List<CommandComponents> that is passed in from CommandHandler() and returns a task if successfully added, throws an exception if it fails to do so (i.e. If a duplicate task is found within)

--- dataManipulation/TaskAdder.java
+++ dataManipulation/TaskAdder.java
@@ -1,36 +1,127 @@
 package dataManipulation;
 
-import java.util.Collections;
-
-import userInterface.ezCMessages;
 import fileIo.FileIo;
+import globalClasses.CommandComponent;
+import globalClasses.Date;
 import globalClasses.Task;
 import globalClasses.ezC;
 
+import java.util.Collections;
+import java.util.List;
+
 public class TaskAdder {
 	
-	// NELSON
-	// Add task to overall list
-	// Keep list AND file sorted by deadline
-	// Deny duplicates to be added
 	/**
 	 * 
-	 * @param toBeAdded the task which will be added to the list
+	 * For the add method:
+	 * @param newTask: the task which will be added to the list
 	 * @param totalTaskList	the list to add the task to
 	 * @param IoStream The stream to rewrite the file afterwards
-	 * @return the task which was added to the list
+	 * @return newTask if task can be added, throws an exception if it cannot be.
+	 * @throws Exception is returned if duplicate task exists
 	 */
-	public static void doAddTask(Task newTask, FileIo IoStream) {
+	
+	private static String taskName = null;
+	private static String taskCategory = null;
+	private static String taskLocation = null;
+	private static String taskNote = null;
+	private static Date taskStart = null;
+	private static Date taskEnd = null;
+	
+	public static Task add(List<CommandComponent> taskAttributes) throws Exception {
+
+		Task newTask = buildTask(taskAttributes);
+
 		if(!ezC.totalTaskList.contains(newTask)) {	// If the list doesn't contain this task, add it
 			ezC.totalTaskList.add(newTask);
-			Collections.sort(ezC.totalTaskList, new globalClasses.sortTask());
+			Collections.sort(ezC.totalTaskList, new globalClasses.sortTaskByEndDate());
+			FileIo IoStream = new FileIo();
 			IoStream.rewriteFile(ezC.totalTaskList);
+			return newTask;
 		}
-		
+
 		else {
-			ezCMessages.printErrorMessage("duplicate add task");
+			throw new Exception("Duplicate Task Found");
+		}
+
+	}
+	
+	/** For the buildTask method:
+	 * 
+	 * @param taskAttributes
+	 * @return the task toBeAdded
+	 */
+	
+	public static Task buildTask(List<CommandComponent> taskAttributes) {
+		
+		assembleAttributes(taskAttributes);
+		
+		Task toBeAdded = TaskFactory.makeTask(taskName, taskCategory, taskLocation, taskNote, taskStart, taskEnd);
+		
+		return toBeAdded;
+		
+	}
+	
+	/** For the aseembleAttributes method:
+	 * 
+	 * @param taskAttributes
+	 */
+	
+	public static void assembleAttributes(List<CommandComponent> taskAttributes) {
+		
+		for(CommandComponent cc : taskAttributes) {
+
+			switch (cc.getType()) {
+
+			case NAME:	setTaskName(cc.getContents());
+						break;
+			
+			case CATEGORY:	setTaskCategory(cc.getContents());
+							break;
+			
+			case LOCATION:	setTaskLocation(cc.getContents());
+							break;
+			
+			case NOTE:	setTaskNote(cc.getContents());
+						break;
+			
+			case START:	setTaskStart(cc.getContents());
+						break;
+			
+			case END:	setTaskEnd(cc.getContents());
+						break;
+			
+			default:
+				break;
+			
+			}
+
 		}
 		
+	}
+	
+	private static void setTaskEnd(String contents) {
+		taskEnd = ezC.determineDate(contents);
+	}
+
+	private static void setTaskStart(String contents) {
+		taskStart = ezC.determineDate(contents);
+	}
+
+	private static void setTaskNote(String contents) {
+		taskNote = contents;
+	}
+
+	private static void setTaskLocation(String contents) {
+		taskLocation = contents;
+	}
+
+	private static void setTaskCategory(String contents) {
+		taskCategory = contents;
+	}
+
+	private static void setTaskName(String contents) {
+		taskName = contents;
 	}
 	
 }
