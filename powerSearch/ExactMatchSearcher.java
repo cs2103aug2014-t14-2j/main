cb24d9d47b3a2667382313643e316c26df37696e
Nelson <nellystix@197-175.priv23.nus.edu.sg>
2014-10-08 19:58:09 +0800

    Re-editing Kash's ExactMatch method.

--- powerSearch/ExactMatchSearcher.java
+++ powerSearch/ExactMatchSearcher.java
@@ -5,26 +5,26 @@ import globalClasses.Task;
 import java.util.ArrayList;
 
 public class ExactMatchSearcher {
-	private String namesearch = new String();
-	private String datesearch = new String();
-	private Date lookfordate;
+	private String nameSearch;
+	private String dateSearch;
+	private Date lookForDate;
 
 	public ExactMatchSearcher(String comm) {
 		if(comm.charAt(2) == '/'){
-			datesearch = comm;
-			datesearch = (datesearch.toLowerCase()).trim();
+			dateSearch = comm;
+			dateSearch = (dateSearch.toLowerCase()).trim();
 			int day = Integer.valueOf(comm.substring(0,2));
 			int month = Integer.valueOf(comm.substring(3,5));
 			int year = Integer.valueOf(comm.substring(6,10));
-			lookfordate = new Date(day, month, year);
+			lookForDate = new Date(day, month, year);
 		}
 		else{
-			namesearch = comm;
-			namesearch = (namesearch.toLowerCase()).trim();
+			nameSearch = comm;
+			nameSearch = (nameSearch.toLowerCase()).trim();
 		}
 	}
 
-	public ArrayList<Task> searchName(ArrayList<Task> tasks){
+	public ArrayList<Task> searchName(String nameSearch){
 		ArrayList<Task> tasksedited = new ArrayList<Task>();
 		int i;
 		for(i=0; i<tasks.size(); i++){
@@ -48,7 +48,7 @@ public class ExactMatchSearcher {
 	
 	public static boolean isExactMatch(Task taskToSearch) {	// Require this method for TaskEditor() method
 
-		return true;
+		
 
 	}
 }
