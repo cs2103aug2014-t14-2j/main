0512ec40ad3b655349cf8ebb63f4129a93bdf538
Nelson <nellystix@197-175.priv23.nus.edu.sg>
2014-10-08 21:09:29 +0800

    Renamed the file from dataManipulation to DataManipulation

--- dataManipulation/dataManipulation.java
+++ dataManipulation/dataManipulation.java
@@ -6,13 +6,13 @@ import globalClasses.*;
 
 
 public class DataManipulation {
-	public static Task add(List<CommandComponent> cc) throws Exception {
-		return TaskAdder.add(cc);
-	}
-	public static Task remove(List<CommandComponent> cc) {
-		return TaskRemover.remove(cc);
-	}
-	public static EditedPair edit(List<CommandComponent> cc) throws Exception {
-		return TaskEditor.edit(cc);
-	}
+  public static Task add(List<CommandComponent> cc) throws Exception {
+    return TaskAdder.add(cc);
+  }
+  public static Task remove(List<CommandComponent> cc) throws Exception {
+    return TaskRemover.remove(cc);
+  }
+  public static EditedPair edit(List<CommandComponent> cc) throws Exception {
+    return TaskEditor.edit(cc);
+  }
 }
