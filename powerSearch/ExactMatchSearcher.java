9aec38891b9135e7846e28a28ee80a43e5f62864
Natalie Rawle <natalierawle@tamu.edu>
2014-10-08 20:11:50 +0800

    made quick searcher

--- powerSearch/ExactMatchSearcher.java
+++ powerSearch/ExactMatchSearcher.java
@@ -1,54 +1,34 @@
 package powerSearch;
 
+import globalClasses.CommandComponent;
 import globalClasses.Date;
 import globalClasses.Task;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class ExactMatchSearcher {
-	private String nameSearch;
-	private String dateSearch;
-	private Date lookForDate;
-
-	public ExactMatchSearcher(String comm) {
-		if(comm.charAt(2) == '/'){
-			dateSearch = comm;
-			dateSearch = (dateSearch.toLowerCase()).trim();
-			int day = Integer.valueOf(comm.substring(0,2));
-			int month = Integer.valueOf(comm.substring(3,5));
-			int year = Integer.valueOf(comm.substring(6,10));
-			lookForDate = new Date(day, month, year);
+	public static List<Task> taskList;
+	
+	public static Task simpleSearch(CommandComponent key, List<Task> list) throws Exception {
+		taskList = list;
+		
+		switch (key.getType()) {
+			case NAME :
+				return simpleSearchName(key.getContents());
+			default :
+				throw new 
+				IllegalArgumentException("invalid subcommand for search");
 		}
-		else{
-			nameSearch = comm;
-			nameSearch = (nameSearch.toLowerCase()).trim();
-		}
-	}
-
-	public ArrayList<Task> searchName(String nameSearch){
-		ArrayList<Task> tasksedited = new ArrayList<Task>();
-		int i;
-		for(i=0; i<tasks.size(); i++){
-			if(tasks.get(i).getName().toLowerCase().trim().equals(namesearch)){
-				tasksedited.add(tasks.get(i));
-			}
-		}
-		return tasksedited;
-	}
-
-	public ArrayList<Task> searchEndDate(ArrayList<Task> tasks){
-		ArrayList<Task> tasksedited = new ArrayList<Task>();
-		int i;
-		for(i=0; i<tasks.size(); i++){
-			if(lookfordate.isBefore(tasks.get(i).getEndDate())){
-				tasksedited.add(tasks.get(i)); //supposed to show all the tasks that have an endDate after the date searched for
-			}
-		}
-		return tasksedited;
 	}
 	
-	public static boolean isExactMatch(Task taskToSearch) {	// Require this method for TaskEditor() method
-
+	private static Task simpleSearchName(String key) throws Exception {
+		for (int i = 0; i < taskList.size(); ++i) {
+			if (key.equals(taskList.get(i).getName())) {
+				return taskList.get(i);
+			}
+		}
 		
-
+		throw new Exception("no matches found");
 	}
 }
