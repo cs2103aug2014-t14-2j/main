ebe8cb6b17e4ee54df0667c01b638d6cc12a6fde
Nelson <nellystix@197-175.priv23.nus.edu.sg>
2014-10-08 19:43:36 +0800

    Moved ExactSearch to ExactMatchSearcher in the powerSearch package

--- powerSearch/ExactMatchSearcher.java
+++ powerSearch/ExactMatchSearcher.java
@@ -1,19 +1,54 @@
 package powerSearch;
 
+import globalClasses.Date;
 import globalClasses.Task;
+import java.util.ArrayList;
 
-import java.util.List;
+public class ExactMatchSearcher {
+	private String namesearch = new String();
+	private String datesearch = new String();
+	private Date lookfordate;
 
-public class ExactMatchSearcher extends Searcher {
+	public ExactMatchSearcher(String comm) {
+		if(comm.charAt(2) == '/'){
+			datesearch = comm;
+			datesearch = (datesearch.toLowerCase()).trim();
+			int day = Integer.valueOf(comm.substring(0,2));
+			int month = Integer.valueOf(comm.substring(3,5));
+			int year = Integer.valueOf(comm.substring(6,10));
+			lookfordate = new Date(day, month, year);
+		}
+		else{
+			namesearch = comm;
+			namesearch = (namesearch.toLowerCase()).trim();
+		}
+	}
 
-	public static Task find(String name) {
-		Task foundInTotalTaskList = new Task("name", "date"); //TOTALLY WRONG PLS FIX
-		return foundInTotalTaskList;
+	public ArrayList<Task> searchName(ArrayList<Task> tasks){
+		ArrayList<Task> tasksedited = new ArrayList<Task>();
+		int i;
+		for(i=0; i<tasks.size(); i++){
+			if(tasks.get(i).getName().toLowerCase().trim().equals(namesearch)){
+				tasksedited.add(tasks.get(i));
+			}
+		}
+		return tasksedited;
+	}
+
+	public ArrayList<Task> searchEndDate(ArrayList<Task> tasks){
+		ArrayList<Task> tasksedited = new ArrayList<Task>();
+		int i;
+		for(i=0; i<tasks.size(); i++){
+			if(lookfordate.isBefore(tasks.get(i).getEndDate())){
+				tasksedited.add(tasks.get(i)); //supposed to show all the tasks that have an endDate after the date searched for
+			}
+		}
+		return tasksedited;
 	}
 	
 	public static boolean isExactMatch(Task taskToSearch) {	// Require this method for TaskEditor() method
-		
+
 		return true;
-		
+
 	}
 }
