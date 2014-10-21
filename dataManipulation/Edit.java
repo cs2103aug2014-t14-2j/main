5319f26ff0954658a559ee1e1b2b0178f698f585
Nelson <nellystix@196-32.priv23.nus.edu.sg>
2014-10-22 00:22:58 +0800

    Rewrote the Edit class, moved relevant code from TaskEditor to Edit.

--- dataManipulation/Edit.java
+++ dataManipulation/Edit.java
@@ -2,11 +2,19 @@ package dataManipulation;
 
 import java.util.List;
 
-import dataEncapsulation.EditedPair;
+import powerSearch.ExactMatchSearcher;
+import powerSearch.Searcher;
+import userInterface.ezCMessages;
+import dataEncapsulation.ActionException;
+import dataEncapsulation.Date;
 import dataEncapsulation.Task;
 import dataEncapsulation.UndoRedoProcessor;
+import fileIo.FileIo;
 
 public class Edit extends Command {
+	
+	private static List<Task> taskList = TotalTaskList.getInstance().getList();
+	private static ezCMessages messages = ezCMessages.getInstance();
 
 	public Edit(List<Subcommand> commandComponents)
 			throws IllegalArgumentException {
@@ -14,19 +22,79 @@ public class Edit extends Command {
 	}
 
 	@Override
-	public String execute() {
-		Task toEdit = searchTaskByName(taskAttributes);
+	public String execute() throws Exception {
+		Task toEdit = getTaskToEdit();
 		Task preEdit = toEdit;
-		Task postEdit = editTask(toEdit, taskAttributes);
+		Task postEdit = editTask(toEdit, subcommands);
 		
 		addEditedTask(preEdit, postEdit);
 		UndoRedoProcessor.preEditTaskStack.add(toEdit);	// Add the pre-edited task into the pre edited task stack
-		UndoRedoProcessor.postEditTaskStack.add(postEdit);	// Add the edited task into the post edited task stack
 		
-		return new EditedPair(preEdit, postEdit);
+		String editComplete = messages.getEditMessage(preEdit, postEdit);
+		return editComplete;
 	}
 	
+	private Task getTaskToEdit() throws Exception {
+		
+		List<Task> tasks = Searcher.search(subcommands, taskList);
+		if(tasks.size() > 1) {
+			ActionException moreThanOne = new ActionException(taskList, ActionException.ErrorLocation.EDIT, subcommands);
+			throw moreThanOne;
+		}
+		else {
+			return tasks.get(0);
+		}
+	}
 	
+	private static Task editTask(Task toEdit, List<Subcommand> taskAttributes) throws Exception {
+		
+		Task editTaskExceptName = Add.buildTask(taskAttributes);
+		Task editTaskIncludingName = setTaskAttributes(editTaskExceptName, taskAttributes);
+		
+		if(ExactMatchSearcher.isTaskDuplicate(editTaskIncludingName)) {
+			ActionException moreThanOne = new ActionException(taskList, ActionException.ErrorLocation.EDIT, taskAttributes);
+			throw moreThanOne;
+		}
+		else {
+			return editTaskIncludingName;
+		}
+	}
+	
+	private static Task setTaskAttributes(Task toEdit, List<Subcommand> taskAttributes) {
+		
+		for(Subcommand cc : taskAttributes) {
+
+			switch (cc.getType()) {
+
+			case TITLE:	toEdit.setName(cc.getContents());
+						break;
+
+			case CATEGORY:	toEdit.setCategory(cc.getContents());
+							break;
+
+			case LOCATION:	toEdit.setLocation(cc.getContents());
+							break;
+
+			case END:	toEdit.setEndDate(Date.determineDate(cc.getContents()));
+						break;
+
+			case NOTE:	toEdit.setNote(cc.getContents());
+						break;
+
+			default:
+				break;
+			}
+		}
+			
+			return toEdit;
+	}
+	
+	public static void addEditedTask(Task oldTask, Task newTask) {
+		taskList.remove(oldTask);
+		taskList.add(newTask);
+		FileIo IoStream = FileIo.getInstance();
+		IoStream.rewriteFile();
+	}
 
 	@Override
 	protected void checkValidity() {
