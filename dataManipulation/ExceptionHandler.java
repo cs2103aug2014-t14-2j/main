082e85b3b29d43ead34dc07942c76b0c0ab6ec4d
Yui Wei <yuiwei@206-1.priv23.nus.edu.sg>
2014-10-21 22:00:55 +0800

    writing exception handler.

--- dataManipulation/ExceptionHandler.java
+++ dataManipulation/ExceptionHandler.java
@@ -1,5 +1,58 @@
 package dataManipulation;
 
-public class ExceptionHandler {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import userInterface.ezCMessages;
+import dataEncapsulation.ActionException;
+
+public class ExceptionHandler {
+	
+	private static Scanner inputSc = new Scanner(System.in);
+	
+	public static String furtherAction(ActionException e) {
+		String ofTasks = ezCMessages.getStringOfTasks(e.getOptions());
+		System.out.println("That was a bit too vague -please choose which of these you would like to " 
+							+ e.getLocation().toString() + ": ");
+		System.out.println(ofTasks);
+		String userChoice = inputSc.nextLine();
+		ArrayList<Integer> choices = getChoices(userChoice);
+		String cmdHandlerOut;
+		switch(e.getLocation()) {
+		case DELETE:
+			cmdHandlerOut = deleteFurther(choices);
+			break;
+		case EDIT:
+			cmdHandlerOut = editFurther(choices);
+			break;
+		default:
+			cmdHandlerOut = "wat";
+			break;
+		}
+		return cmdHandlerOut;
+	}
+	public static editFurther(ArrayList<Integer> ch) {
+		for (int i : ch) {
+			
+		}
+	}
+	
+	public static deleteFurther(ArrayList<Integer> ch) {
+		for (int i : ch) {
+			
+		}
+	}
+	
+	public static ArrayList<Integer> getChoices(String input) {
+		String str = input;
+		if(input.contains(",")) {
+			str.replaceAll(",", " ");
+		}
+		String choices[] = str.split(" ");
+		ArrayList<Integer> ch = new ArrayList<Integer>();
+		for(String s : choices) {
+			ch.add(Integer.parseInt(s));
+		}
+		return ch;
+	}
 }
