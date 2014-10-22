f29a92912a23ebf046b4ec79a1b49c13ebf0178a
Nelson <nellystix@198-156.priv23.nus.edu.sg>
2014-10-23 04:03:05 +0800

    Rewrote Add Test. Working and fine.

---
+++ dataManipulation/AddTest.java
@@ -0,0 +1,67 @@
+package dataManipulation;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import dataEncapsulation.Date;
+import dataEncapsulation.Task;
+
+public class AddTest {
+
+	String taskName = null;
+	String taskCategory = null;
+	String taskLocation = null;
+	String taskNote = null;
+	Date taskStart = null;
+	Date taskEnd = null;
+	
+	@Test
+	public void testBuildTask() {
+		List<Subcommand> listCC = new ArrayList<Subcommand>();
+		listCC.add(new Subcommand(Subcommand.TYPE.NAME, "Buy Milk"));
+		listCC.add(new Subcommand(Subcommand.TYPE.CATEGORY, "Groceries"));
+		listCC.add(new Subcommand(Subcommand.TYPE.START, "01/01/2014"));
+		listCC.add(new Subcommand(Subcommand.TYPE.END, "02/01/2014"));
+		listCC.add(new Subcommand(Subcommand.TYPE.LOCATION, "Clementi"));
+		listCC.add(new Subcommand(Subcommand.TYPE.NOTE, "Meiji Chocolate Milk"));
+		
+		Task actual = new Add(listCC).buildTask(listCC);
+		Date startDate = Date.determineDate("01/01/2014");
+		Date endDate = Date.determineDate("02/01/2014");
+		Task expected = new Task("Buy Milk", "Groceries", "Clementi", "Meiji Chocolate Milk", startDate, endDate);
+		
+		assertEquals("Task name matches expected", expected.getName(), actual.getName());
+		assertEquals("Task location matches expected", expected.getLocation(), actual.getLocation());
+		assertEquals("Task note matches as expected", expected.getNote(), actual.getNote());
+		assertEquals("Task category matches as expected", expected.getCategory(), actual.getCategory());
+		assertEquals("Task start date matches as expected", expected.getStartDate().toString(), actual.getStartDate().toString());
+		assertEquals("Task end date matches as expected", expected.getEndDate().toString(), actual.getEndDate().toString());
+	}
+	
+	@Test
+	public void testAdd() throws Exception {
+		List<Subcommand> listCC = new ArrayList<Subcommand>();
+		listCC.add(new Subcommand(Subcommand.TYPE.NAME, "Buy Milk"));
+		listCC.add(new Subcommand(Subcommand.TYPE.CATEGORY, "Groceries"));
+		listCC.add(new Subcommand(Subcommand.TYPE.START, "01/01/2014"));
+		listCC.add(new Subcommand(Subcommand.TYPE.END, "02/01/2014"));
+		listCC.add(new Subcommand(Subcommand.TYPE.LOCATION, "Clementi"));
+		listCC.add(new Subcommand(Subcommand.TYPE.NOTE, "Meiji Chocolate Milk"));
+		Date startDate = Date.determineDate("01/01/2014");
+		Date endDate = Date.determineDate("02/01/2014");
+		Task expected = new Task("Buy Milk", "Groceries", "Clementi", "Meiji Chocolate Milk", startDate, endDate);
+		
+		Task actual = new Add(listCC).buildTask(listCC);
+		
+		assertEquals("Task name matches expected", expected.getName(), actual.getName());
+		assertEquals("Task location matches expected", expected.getLocation(), actual.getLocation());
+		assertEquals("Task note matches as expected", expected.getNote(), actual.getNote());
+		assertEquals("Task category matches as expected", expected.getCategory(), actual.getCategory());
+		assertEquals("Task start date matches as expected", expected.getStartDate().toString(), actual.getStartDate().toString());
+		assertEquals("Task end date matches as expected", expected.getEndDate().toString(), actual.getEndDate().toString());
+	}
+}
