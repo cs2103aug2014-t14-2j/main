a0804d9595925177da2536102aae6441378b7624
Kaushik Suresh <kaushikiyersuresh@gmail.com>
2014-10-23 10:32:45 +0800

    Changed some vital code

--- powerSearch/Searcher.java
+++ powerSearch/Searcher.java
@@ -6,7 +6,6 @@ import java.util.List;
 import dataEncapsulation.Task;
 import dataManipulation.Subcommand;
 
-
 public class Searcher {
 	/**
 	 * searches for exact/near matches to search key
@@ -21,6 +20,7 @@ public class Searcher {
 		assert(list!=null);
 		String command = new String();
 		for(i=0; i<keylist.size(); i++){
+			//System.out.println(keylist.get(i));
 			switch(keylist.get(i).getType()){
 			case AND:
 				command = "AND";
@@ -30,6 +30,7 @@ public class Searcher {
 				break;
 			default:
 				answer.add(ExactMatchSearcher.exactSearch(keylist.get(i), list));
+				answer.add(NearMatchSearcher.nearSearch(keylist.get(i), list));
 			}
 		}
 		List<Task> results = new ArrayList<Task>();
@@ -43,20 +44,6 @@ public class Searcher {
 		default:
 			results = mergeListsOR(answer);
 		}
-		if(results.isEmpty()){
-			for(i=0; i<keylist.size(); i++){
-				switch(keylist.get(i).getType()){
-				case AND:
-					command = "AND";
-					break;
-				case OR:
-					command = "OR";
-					break;
-				default:
-					answer.add(NearMatchSearcher.nearSearch(keylist.get(i), list));
-				}
-			}
-		}
 		return results;
 	}
 
@@ -64,25 +51,40 @@ public class Searcher {
 		if(answer.isEmpty())
 			return null;
 		List<Task> list = new ArrayList<Task>();
-		int i, j, k;
-		List<Task> comp = new ArrayList<Task>();
 		list = answer.get(0);
-		for(i=1; i<answer.size(); i++){
-			comp = answer.get(i); 
-			for(j=0; j< list.size(); j++){
-				k = 0;
-				while(k<=comp.size()){
-					if(!list.get(j).equals(comp.get(k))){
-						list.add(comp.get(k));
-					}
-					k++;
-				}	
-			}
+		for(int i = 1; i < answer.size(); i++){
+			list.addAll(answer.get(i));
 		}
-		if(list.isEmpty())
+		if(list.size()>=2){
+			list = removeDuplicates(list);
+			return list;
+		}
+		else if(list.isEmpty())
 			return null;
 		else
 			return list;
+	}
+	
+	private static List<Task> removeDuplicates(List<Task> list){
+		int k=0;
+		Task t1;
+		while(k<=list.size()-1){
+			if(list.get(k)!=null)
+				t1 = list.get(k);
+			else
+				t1 = list.get(++k);
+			for(int i = 0; i < list.size(); i++){
+				if(list.get(i)!=null){
+					if(t1.equals(list.get(i)) && i!=k){
+						list.remove(i);
+					}
+				}
+				else
+					list.remove(i);
+			}
+			k++;
+		}
+		return list;
 	}
 
 	private static List<Task> mergeListsAND(List<List<Task>> answer){
