6307d677f01378ef152319c562af1524405343c7
Kaushik Suresh <kaushikiyersuresh@gmail.com>
2014-11-03 10:18:49 +0800

    Update SearcherTest.java

--- powerSearch/SearcherTest.java
+++ powerSearch/SearcherTest.java
@@ -1,4 +1,4 @@
-//@ Kaushik A0108297X
+//@author Kaushik A0108297X
 package powerSearch;
 
 import static org.junit.Assert.*;
@@ -32,9 +32,12 @@ public class SearcherTest {
 	public void testSearchTimeSlots() throws Exception {
 		List<Task> tasks = new ArrayList<Task>();
 		Date temp1 = new Date();
-		Date end1 = new Date(31,10,2014,23,0,0);
+		Date end1 = new Date(3,11,2014,12,30,0);
+		Date start2 = new Date(3, 11, 2014, 18, 45, 0);
+		Date end2 = new Date(3, 11, 2014, 23, 45, 0);
 		String location = new String("CSC");
 		tasks.add(new Task("Andrew", "A", location, temp1, end1));
+		tasks.add(new Task("Bravo", "B", location, start2, end2));
 		//tasks.add(new Task("Bravo", "B", temp1, new Date(26,9,2015)));
 		//tasks.add(new Task("Charlie", "C", temp1, new Date(25,12,2014)));
 		
