ce568a9345cd26b334ef6e0aea2f736b343efa68
Nelson <nellystix@181-123.priv23.nus.edu.sg>
2014-11-06 00:50:56 +0800

    Removed a line that used to push in commands into the Undo List based on the user's command input instead of from the command itself

--- userInterface/CommandHandlingAction.java
+++ userInterface/CommandHandlingAction.java
@@ -39,7 +39,6 @@ public class CommandHandlingAction extends AbstractAction {
 			Command command = interpreter.formCommand(input);
 			String feedback = command.execute();
 			entry.setText("");
-			UndoRedoList.getInstance().pushUndoCommand(command);	// Adds the command to the undo command stack
 
 			textArea.setText(feedback);
 		} catch (ActionException e) {
