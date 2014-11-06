79b3dfe7dc135e26e8d590cec4610a0bdc6486bf
Kaushik Suresh <kaushikiyersuresh@gmail.com>
2014-11-06 11:08:16 +0800

    Update SearcherTest.java

--- powerSearch/SearcherTest.java
+++ powerSearch/SearcherTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 
 import dataEncapsulation.Date;
 import dataEncapsulation.Task;
+import dataEncapsulation.Time;
 import dataManipulation.Subcommand;
 
 public class SearcherTest {
@@ -35,15 +36,17 @@ public class SearcherTest {
 	@Test
 	public void testSearchTimeSlots() throws Exception {
 		List<Task> tasks = new ArrayList<Task>();
-		Date temp1 = new Date();
-		Date end1 = new Date(3,11,2014,12,30,0);
-		Date start2 = new Date(3, 11, 2014, 18, 45, 0);
-		Date end2 = new Date(3, 11, 2014, 23, 45, 0);
+		Date temp1 = new Date(7, 11, 2014);
+		Date end1 = new Date(7,11,2014);
+		Date start2 = new Date(7, 11, 2014);
+		Date end2 = new Date(7, 11, 2014);
+		Time time1 = new Time(7, 0);
+		Time time2 = new Time(11, 45);
+		Time time3 = new Time(18, 40);
+		Time time4 = new Time(23, 0);
 		String location = new String("CSC");
-		tasks.add(new Task("Andrew", "A", location, temp1, end1));
-		tasks.add(new Task("Bravo", "B", location, start2, end2));
-		//tasks.add(new Task("Bravo", "B", temp1, new Date(26,9,2015)));
-		//tasks.add(new Task("Charlie", "C", temp1, new Date(25,12,2014)));
+		tasks.add(new Task("Andrew", "A", location, temp1, end1, time1, time2));
+		tasks.add(new Task("Bravo", "B", location, start2, end2, time3, time4));
 		
 		System.out.println(Searcher.searchTimeSlot(tasks, temp1));
 	}
