f9e8986e23750970474faf31bda05fdd9f447b1e
Nelson <nellystix@198-216.priv23.nus.edu.sg>
2014-11-06 20:08:43 +0800

    Added more lines for Finish, not done fixing yet

--- dataManipulation/Finish.java
+++ dataManipulation/Finish.java
@@ -1,5 +1,6 @@
 package dataManipulation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import powerSearch.Searcher;
@@ -38,14 +39,29 @@ public class Finish extends Command {
 		TotalTaskList list = TotalTaskList.getInstance();
 		FileIo IoStream = FileIo.getInstance();
 		
-		list.remove(oldTask);
-		list.add(newTask);
+		list.removeNotCompleted(oldTask);
+		list.addCompleted(newTask);
 		IoStream.rewriteFile();
 	}
 	
 	private Task searchTaskByName(List<Subcommand> taskAttributes) throws Exception {
-		TotalTaskList list = TotalTaskList.getInstance();
-		List<Task> toEditArray = Searcher.search(taskAttributes, list.getList());
+		
+		List<Task> completedTasks = TotalTaskList.getInstance().getCompleted();
+		List<Task> currentTasks = TotalTaskList.getInstance().getList();
+		List<Task> overdueTasks = TotalTaskList.getInstance().getOverdue();
+		List<List<Task>> categorizedTasks = new ArrayList<List<Task>>();
+		categorizedTasks.add(completedTasks);
+		categorizedTasks.add(currentTasks);
+		categorizedTasks.add(overdueTasks);
+		List<Task> toEditArray = new ArrayList<Task>();
+		
+		for(List<Task> lt : categorizedTasks) {
+			toEditArray = Searcher.search(taskAttributes, lt);
+			if(toEditArray != null) {
+				break;
+			}
+		}
+		
 		if(toEditArray.size() > 1) {
 			throw new Exception("Too many searches returned, need a more specific task to edit.");
 		} else if (toEditArray.size() == 0) {
