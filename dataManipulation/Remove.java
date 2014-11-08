83608d469937b787f49870d1f9cb388dfb803e5d
Nelson <nellystix@196-145.priv23.nus.edu.sg>
2014-11-08 20:07:08 +0800

    Write code for removing specially for edit

--- dataManipulation/Remove.java
+++ dataManipulation/Remove.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import powerSearch.ExactMatchSearcher;
+import powerSearch.Searcher;
 import dataEncapsulation.ActionException;
 import dataEncapsulation.BadCommandException;
 import dataEncapsulation.BadSubcommandException;
@@ -16,6 +17,7 @@ public class Remove extends Command {
 	private static List<Task> tasksFound;
 	private static Task taskToRemove;
 	private static Task taskRemoved;
+	private static Task taskRemovedForEdit;
 	private static List<Subcommand> sc;
 
 	public Remove(List<Subcommand> commandComponents)
@@ -30,12 +32,23 @@ public class Remove extends Command {
 		taskRemoved = remove(sc);
 		return "Just deleted: \n" + taskRemoved.toString();
 	}
-
+	
 	@Override
 	protected void checkValidity() throws BadSubcommandException {
 		super.checkValidity();
 		checkForNoDuplicateSubcommands();
 	}
+	
+	/*
+	 * @author Nelson / A0111014J
+	 */
+	
+	public void executeForEdit() throws Exception {
+		
+		removeForEdit(sc);
+		
+	}
+	
 	public static Task remove(List<Subcommand> cc) throws Exception {
 		
 		tasksFound = new ArrayList<Task>();
@@ -46,14 +59,15 @@ public class Remove extends Command {
 		categorizedTasks.add(completedTasks);
 		categorizedTasks.add(currentTasks);
 		categorizedTasks.add(overdueTasks);
-		int i = 0;
-				
-		for(i = 0; i < categorizedTasks.size(); i++) {
-			Task exactTask = ExactMatchSearcher.exactTaskSearch(cc, categorizedTasks.get(i));
-			if(exactTask != null) {
-				tasksFound.add(exactTask);
-				break;
+		int j = 0;
+		
+		for(int i = 0; i < categorizedTasks.size(); i++) {
+			List<Task> tasksThusFar = Searcher.search(cc, categorizedTasks.get(i));
+			if(tasksThusFar.size() != 0) {
+				tasksFound.addAll(tasksThusFar);
+				j = i;
 			}
+			
 		}
 		
 		if (tasksFound.size() > 1) {
@@ -63,10 +77,35 @@ public class Remove extends Command {
 		}
 		taskToRemove = tasksFound.get(0) ;
 		taskRemoved = taskToRemove;
-		doDeleteTask(taskToRemove, i);
+		doDeleteTask(taskToRemove, j);
 		return taskRemoved;
 		
 	}
+	
+	public static void removeForEdit(List<Subcommand> listOfSubs) {
+		
+		Task exactTask = null;
+		List<Task> completedTasks = TotalTaskList.getInstance().getCompleted();
+		List<Task> currentTasks = TotalTaskList.getInstance().getList();
+		List<Task> overdueTasks = TotalTaskList.getInstance().getOverdue();
+		List<List<Task>> categorizedTasks = new ArrayList<List<Task>>();
+		categorizedTasks.add(completedTasks);
+		categorizedTasks.add(currentTasks);
+		categorizedTasks.add(overdueTasks);
+		int i = 0;
+		
+		for(i = 0; i < categorizedTasks.size(); i++) {
+			Task exactTaskFound = ExactMatchSearcher.exactTaskSearch(listOfSubs, categorizedTasks.get(i));
+			if(exactTaskFound != null) {
+				exactTask = exactTaskFound;
+				break;
+			}
+		}
+		
+		doDeleteTask(exactTask, i);
+		
+	}
+	
 	public static Task doDeleteTask(Task toRemove, int i) {
 		Task a = toRemove;
 		
@@ -81,6 +120,10 @@ public class Remove extends Command {
 				
 			case 2 :
 				TotalTaskList.getInstance().removeOverdue(a);
+				break;
+				
+			default:
+				System.out.println("Delete failed");
 				break;
 				
 		}
